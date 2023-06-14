1495f090e244d91616e85be18d797685e7eaecac
Mahesh Bhise <mbhise431@gmail.com>
2023-06-14 11:12:59 +0530

    Updated get personal fields call to filter by list id

--- lib/rav_meser_v2/personal_field.rb
+++ lib/rav_meser_v2/personal_field.rb
@@ -1,11 +1,15 @@
 module RavMeserV2
   module PersonalField
-    def get_personal_fields
-      send_request(:get, "/personal-fields")
+    def get_personal_fields(list_id = nil)
+      if list_id
+        send_request(:get, "/lists/#{list_id}/personal-fields")
+      else  
+        send_request(:get, "/personal-fields")
+      end
     end
 
     def create_personal_field(list_id, query_params = {})
-      send_request(:post, "/personal-fields", query_params.merge({list_ids: [list_id]}))
+      send_request(:post, "/personal-fields", query_params.merge({list_id: list_id}))
     end
 
     def update_personal_field(field_id, query_params = {})
